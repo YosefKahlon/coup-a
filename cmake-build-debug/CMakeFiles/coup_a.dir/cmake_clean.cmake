5bc861e55a115b2280a733a42c3207679ab79b85
yosefkahlon <yosefkahlon53@gmail.com>
2022-04-25 14:58:06 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/coup_a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coup_a.dir/main.cpp.o"
+  "coup_a"
+  "coup_a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coup_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
